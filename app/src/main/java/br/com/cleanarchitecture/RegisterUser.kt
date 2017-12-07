24794a41f7bfeacc4065e933da21b5f89ed81eaf
Thalisson Estrela <thalissondev@gmail.com>
2017-12-06 22:53:31 -0200

    Initialize tests of implementations

---
+++ app/src/main/java/br/com/cleanarchitecture/RegisterUser.kt
@@ -0,0 +1,12 @@
+package br.com.cleanarchitecture
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterUser : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register_user)
+    }
+}
